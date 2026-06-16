d7ce82abdbec0565a19a62fd55ca4ecf2c542d2f
Snehal Arbooj <snehalarbooj03@gmail.com>
2026-06-17 00:13:04 +0530

    Prepare Day 4 for deployment

--- Day4_SentimentAnalysis/app.py
+++ Day4_SentimentAnalysis/app.py
@@ -1,23 +1,34 @@
 from flask import Flask, render_template, request
 import joblib
+import os
 
 app = Flask(__name__)
 
-result = None
-sentiment_class = ""
-review = ""
-confidence = None
+# ============================
+# Load Model and Vectorizer
+# ============================
 
-# Load model and vectorizer
-model = joblib.load("sentiment_model.pkl")
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+model = joblib.load(
+    os.path.join(BASE_DIR, "sentiment_model.pkl")
+)
+
+vectorizer = joblib.load(
+    os.path.join(BASE_DIR, "tfidf_vectorizer.pkl")
+)
+
+# ============================
+# Routes
+# ============================
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+
     result = None
     sentiment_class = ""
-
-
+    review = ""
+    confidence = None
 
     if request.method == "POST":
 
@@ -28,9 +39,6 @@ def home():
         prediction = model.predict(vectorized_review)[0]
 
         probs = model.predict_proba(vectorized_review)[0]
-
-        negative_prob = probs[0]
-        positive_prob = probs[1]
 
         confidence = max(probs) * 100
 
@@ -46,7 +54,17 @@ def home():
             result = "😞 Negative Review"
             sentiment_class = "negative"
 
-    return render_template("index.html", result=result, sentiment_class=sentiment_class, review=review , confidence=round(confidence, 2))
+    return render_template(
+        "index.html",
+        result=result,
+        sentiment_class=sentiment_class,
+        review=review,
+        confidence=round(confidence, 2) if confidence else None
+    )
+
+# ============================
+# Run App
+# ============================
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
